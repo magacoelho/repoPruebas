0c7756d9e8f743bc15c7b6356d0d7da29374dc9f
bl0819 <bl0819@192.168.7.10>
2014-10-03 19:40:27 +0200

    Ageregué dos clases

---
+++ src/main/java/es/upm/miw/dos/repositorio/Fraction.java
@@ -0,0 +1,29 @@
+package es.upm.miw.dos.repositorio;
+
+public class Fraction {
+	private int numerator;
+
+private int denominator;
+
+public Fraction(int numerator, int denominator) {
+    this.numerator = numerator;
+    this.denominator = denominator;
+}
+
+public Fraction() {
+    this(1, 1);
+}
+
+public int getNumerator() {
+    return numerator;
+}
+
+public int getDenominator() {
+    return denominator;
+}
+
+public double decimal() {
+    return (double) numerator / denominator;
+}
+
+}
